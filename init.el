030994b377be7f9f0f7da188559c4ca86a6ee668
tunarider <tunarider@tunaground.net>
2025-01-30 00:27:32 +0900

    ts setup

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(package-selected-packages
-   '(treemacs-nerd-icons treemacs-all-the-icons popwin go-mode yaml-mode all-the-icons lsp-java helm-lsp lsp-ui dap-mode yasnippet which-key pinentry company flycheck-pos-tip flycheck vterm-toggle vterm treemacs-magit treemacs-projectile treemacs-evil treemacs helm-projectile ibuffer-projectile projectile highlight-indent-guides nyan-mode solaire-mode centaur-tabs doom-modeline exec-path-from-shell))
+   '(tree-sitter treesit php-mode typescript-mode treemacs-nerd-icons treemacs-all-the-icons popwin go-mode yaml-mode all-the-icons lsp-java helm-lsp lsp-ui dap-mode yasnippet which-key pinentry company flycheck-pos-tip flycheck vterm-toggle vterm treemacs-magit treemacs-projectile treemacs-evil treemacs helm-projectile ibuffer-projectile projectile highlight-indent-guides nyan-mode solaire-mode centaur-tabs doom-modeline exec-path-from-shell))
  '(size-indication-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -112,7 +112,7 @@
   ;; https://github.com/emacs-lsp/lsp-treemacs/issues/89
   ;; (treemacs-load-all-the-icons-with-workaround-font "Hermit")
   (with-eval-after-load 'lsp-treemacs
-    (treemacs-load-all-the-icons-with-workaround-font "Menlo")
+    (treemacs-load-all-the-icons-with-workaround-font "MesloLGS NF")
     ;; (doom-themes-treemacs-config)
     ))
 
@@ -299,6 +299,7 @@
   :init
   (setq lsp-keymap-prefix "C-c l")
   :hook ((go-mode . lsp)
+         (typescript-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
@@ -323,8 +324,82 @@
 
 (use-package go-mode)
 
+(use-package typescript-mode
+  :config
+  (setq typescript-indent-level 2))
+
+
+(use-package tree-sitter
+      :mode (("\\.tsx\\'" . tsx-ts-mode)
+             ("\\.js\\'"  . typescript-ts-mode)
+             ("\\.mjs\\'" . typescript-ts-mode)
+             ("\\.mts\\'" . typescript-ts-mode)
+             ("\\.cjs\\'" . typescript-ts-mode)
+             ("\\.ts\\'"  . typescript-ts-mode)
+             ("\\.jsx\\'" . tsx-ts-mode)
+             ("\\.json\\'" .  json-ts-mode)
+             ("\\.Dockerfile\\'" . dockerfile-ts-mode)
+             ("\\.prisma\\'" . prisma-ts-mode)
+             ;; More modes defined here...
+             )
+      :preface
+      (defun os/setup-install-grammars ()
+        "Install Tree-sitter grammars if they are absent."
+        (interactive)
+        (dolist (grammar
+                 '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+                   (bash "https://github.com/tree-sitter/tree-sitter-bash")
+                   (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+                   (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+                   (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+                   (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+                   (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+                   (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+                   (make "https://github.com/alemuller/tree-sitter-make")
+                   (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+                   (cmake "https://github.com/uyha/tree-sitter-cmake")
+                   (c "https://github.com/tree-sitter/tree-sitter-c")
+                   (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+                   (toml "https://github.com/tree-sitter/tree-sitter-toml")
+                   (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+                   (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+                   (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+                   (prisma "https://github.com/victorhqc/tree-sitter-prisma")))
+          (add-to-list 'treesit-language-source-alist grammar)
+          ;; Only install `grammar' if we don't already have it
+          ;; installed. However, if you want to *update* a grammar then
+          ;; this obviously prevents that from happening.
+          (unless (treesit-language-available-p (car grammar))
+            (treesit-install-language-grammar (car grammar)))))
+
+      ;; Optional, but recommended. Tree-sitter enabled major modes are
+      ;; distinct from their ordinary counterparts.
+      ;;
+      ;; You can remap major modes with `major-mode-remap-alist'. Note
+      ;; that this does *not* extend to hooks! Make sure you migrate them
+      ;; also
+      (dolist (mapping
+               '((python-mode . python-ts-mode)
+                 (css-mode . css-ts-mode)
+                 (typescript-mode . typescript-ts-mode)
+                 (js-mode . typescript-ts-mode)
+                 (js2-mode . typescript-ts-mode)
+                 (c-mode . c-ts-mode)
+                 (c++-mode . c++-ts-mode)
+                 (c-or-c++-mode . c-or-c++-ts-mode)
+                 (bash-mode . bash-ts-mode)
+                 (css-mode . css-ts-mode)
+                 (json-mode . json-ts-mode)
+                 (js-json-mode . json-ts-mode)
+                 (sh-mode . bash-ts-mode)
+                 (sh-base-mode . bash-ts-mode)))
+        (add-to-list 'major-mode-remap-alist mapping))
+      :config
+      (os/setup-install-grammars))
+
 (load-file "~/.emacs.d/local.el")
 
 (provide 'init)
 
 ;;; init.el ends here
+
